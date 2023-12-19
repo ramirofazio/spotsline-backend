6cdc2da905127875fc3a4f6169276a7cbcd1d6bb
Ramiro <ramifazio@gmail.com>
2023-12-18 21:35:33 -0300

    add public route on product controller

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Query, Get, Param } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { Product } from './products.dto';
+import { Public } from 'src/auth/publicDecorator';
 
 @Controller('products')
 export class ProductsController {
   constructor(private productsService: ProductsService) {}
 
+  @Public()
   @Get('pag')
   async getPaginatedProducts(
     @Query('take') take: number,
@@ -14,6 +16,7 @@ export class ProductsController {
     return await this.productsService.getPaginatedProducts(take, skip);
   }
 
+  @Public()
   @Get('/:id')
   async getOneProduct(@Param('id') id: number): Promise<Product> {
     return await this.productsService.getOneProduct(id);
