c15f07b0818d5d7e334bc699d5eb94f4d19117b9
Your Name <ramifazio@gmail.com>
2024-03-13 20:04:15 -0300

    add userProfile Route

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,22 +2,38 @@ import {
   Body,
   Controller,
   Get,
+  Headers,
   HttpStatus,
   Param,
   Patch,
   Post,
+  UnauthorizedException,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import {
   UpdateCurrentAccountDTO,
   OrderBodyDTO,
-  UserOrdersDTO,
   CleanOrders,
 } from './users.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private userService: UsersService) {}
+
+  @Get('profile')
+  async getOneUserData(@Headers('authorization') authorizationHeader: string) {
+    if (!authorizationHeader) {
+      throw new UnauthorizedException();
+    }
+
+    const [bearer, token] = authorizationHeader.split(' ');
+
+    if (bearer !== 'Bearer' || !token) {
+      throw new UnauthorizedException('Invalid authorization header format');
+    }
+
+    return await this.userService.getUserData(token);
+  }
 
   @Get('orders')
   async getUserOrders(@Body() { id }: { id: number }): Promise<CleanOrders[]> {
