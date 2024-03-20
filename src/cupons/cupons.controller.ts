e8cde6d9ede2096255f4dfa353172b27cd303668
TomasPerez1 <101677692+TomasPerez1@users.noreply.github.com>
2024-03-20 11:34:04 -0300

    finish routes

--- src/cupons/cupons.controller.ts
+++ src/cupons/cupons.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, HttpStatus, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { CuponsService } from './cupons.service';
 import { CreateCupon, ChangeState, Cupon } from './cupons.dto';
 import { Public } from 'src/auth/publicDecorator';
@@ -8,15 +8,26 @@ export class CuponsController {
   constructor(private readonly CuponsService: CuponsService) {}
 
   @Public()
+  @Get("")
+  async getCupons():Promise<Cupon[]> {
+    return await this.CuponsService.getCupons()
+  }
+
+  @Public()
   @Post("/create")
   async createCupon(@Body() body: CreateCupon):Promise<Cupon> {
     return await this.CuponsService.createCupon(body)
   }
 
   @Public()
-  @Post("/change_state")
-  async changeState(@Body() body: ChangeState) {
+  @Put("/change_state")
+  async changeState(@Body() body: ChangeState): Promise<Cupon> {
     return await this.CuponsService.changeState(body)
   }
 
+  @Public()
+  @Delete("/delete/:cuponId")
+  async deleteCupon(@Param("cuponId") cuponId: number) {
+    return await this.CuponsService.deleteCupon(cuponId)
+  }
 }
--- src/cupons/cupons.service.ts
+++ src/cupons/cupons.service.ts
@@ -6,6 +6,10 @@ import { PrismaService } from 'src/prisma/prisma.service';
 export class CuponsService {
   constructor(private readonly prisma: PrismaService) {}
 
+  async getCupons(): Promise<Cupon[]> {
+    return await this.prisma.cupons.findMany()
+  } 
+
   async createCupon({ discountPercentaje, name }: CreateCupon): Promise<Cupon> {
     try {
       const cupon = await this.prisma.cupons.create({
@@ -38,7 +42,24 @@ export class CuponsService {
       HttpStatus.ACCEPTED;
       return updatedCupon;
     } catch (err) {
-      console.log(err)
+      console.log(err);
+      throw new HttpException(err.message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+  async deleteCupon(cuponId: number): Promise<Cupon> {
+    try {
+      await this.prisma.cupons.findFirstOrThrow({ where: { id: cuponId } });
+
+      const deletedCupon = await this.prisma.cupons.delete({
+        where: {
+          id: cuponId,
+        }
+      });
+
+      HttpStatus.ACCEPTED;
+      return deletedCupon;
+    } catch (err) {
+      console.log(err);
       throw new HttpException(err.message, HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
