0506cbff2ef17fc9756c31cafa354ebd91a0a0ad
Your Name <ramifazio@gmail.com>
2024-04-08 12:53:31 -0300

    Sellers page

--- src/seller/seller.controller.ts
+++ src/seller/seller.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, HttpStatus, Patch } from '@nestjs/common';
+import { SellerProfileResponse, AddEmailBodyDTO } from './sellers.dto';
+import { SellerService } from './seller.service';
 
-@Controller('seller')
-export class SellerController {}
+@Controller('sellers')
+export class SellerController {
+  constructor(private sellerService: SellerService) {}
+
+  @Patch('add-email')
+  async addEmailToSeller(@Body() body: AddEmailBodyDTO): Promise<HttpStatus> {
+    return await this.sellerService.addEmailToSeller(body);
+  }
+
+  @Get('/dashboard-sellers')
+  async getDashboardSellers(): Promise<SellerProfileResponse[]> {
+    return await this.sellerService.getDashboardSellers();
+  }
+}
