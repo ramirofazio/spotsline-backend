7a919e6d88d4a67fc201905b0cb30bec8baafa80
tomasPerez <tomasperez.developer@gmail.com>
2024-04-16 19:30:02 -0300

    add update image

--- src/aws/aws.controller.ts
+++ src/aws/aws.controller.ts
@@ -13,12 +13,13 @@ import {
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { AwsService } from './aws.service';
+import { Public } from 'src/auth/publicDecorator';
 
 @Controller('aws-s3-upload')
 export class AwsController {
   constructor(private readonly awsService: AwsService) {}
 
-  @Post('/:variant_id')
+  @Post('productImg/:variant_id')
   @UseInterceptors(FileInterceptor('file'))
   async uploadproductImage(
     @UploadedFile(
@@ -33,5 +34,24 @@ export class AwsController {
     @Param('variant_id') variant_id: number,
   ): Promise<HttpStatus> {
     return await this.awsService.uploadProductImage(file, variant_id);
+  }
+
+  @Public()
+  @Post('avatar/:user_id')
+  @UseInterceptors(FileInterceptor('file'))
+  async updateAvatar(
+    @Query('web_role') web_role: string,
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({ maxSize: 31457280 }),
+          new FileTypeValidator({ fileType: '.(png|jpeg|jpg)' }),
+        ],
+      }),
+    )
+    file: Express.Multer.File,
+    @Param('user_id') user_id: number,
+  ): Promise<HttpStatus> {
+    return await this.awsService.updateAvatar(file, user_id, web_role);
   }
 }
