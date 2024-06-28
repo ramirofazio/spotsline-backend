d9de32c630fb7993a2074a4b91869dcd0161b360
ramiro <ramifazio@gmail.com>
2024-06-27 21:47:05 -0300

    clean & fix

--- src/mobbex/mobbex.service.ts
+++ src/mobbex/mobbex.service.ts
@@ -21,11 +21,12 @@ export class MobbexService {
   async webhookResponse(data: any) {
     try {
       console.log('WEBHOOK DATA RAW', data);
+      const status = data.status.code || data.payment.status.code;
 
-      if (data.payment.status.code !== '200') {
+      if (status !== '200') {
         //? EN ESTE BLOQUE A FUTURO SE PUEDEN HACER COSITAS DE EMAIL MARKETING U OTROS FLUJOS CUANDO EL PAGO FALLO
         console.log('PAGO FALLIDO');
-        const orderId = data.checkout.reference;
+        const orderId = data.payment.reference || data.checkout.reference;
 
         //? Elimino la orden temporal creada porque fallo el pago
         await this.prisma.web_orders.delete({
@@ -51,23 +52,6 @@ export class MobbexService {
       const type = data.payment.source.type;
 
       console.log('WEBHOOK DATA:', userId, transactionId, type, orderId);
-
-      //   //? Recupero los items temporales guardados
-      //   const items = await this.prisma.checkout_items.findMany({
-      //     where: { userId },
-      //   });
-
-      //   if (!items) {
-      //     throw new HttpException(
-      //       'No hay items guardados del checkout',
-      //       HttpStatus.NOT_FOUND,
-      //     );
-      //   }
-
-      //   const cleanItems = items.map((i) => {
-      //     return { productId: i.productId, qty: i.productQty };
-      //   });
-
       console.log('llegue aca?');
 
       //TODO ACA TENGO QUE UPDATEAR LA ORDEN. PUEDO FILTRAR CON LA ULTIMA CREADA  y el total capaz
@@ -79,33 +63,6 @@ export class MobbexService {
       console.log('UPDATEE ORDEN TEMPORAL');
 
       //TODO ACA MANDAR MAIL Y CREAR SYSTEM ORDER
-
-      /*
-! una opcion puede ser crear la orden con una propiedad en false tipo estado,
-! entonces aca agarro esta orden y la modifcio nomas.
-! Hay que ver como conseguir los datos del envio.
-! Probablemente se tendrian que mandar a la hora de hacer el checkout para directamente crear una orden temporal al momento de crear el checkout.
-! A analizar...
- */
-
-      //! DEPRECADO
-      //? CReo la orden, falta recuperar los datos del descuento y delivery
-      //   await this.usersService.createOrder({
-      //     userId,
-      //     items: cleanItems,
-      //     transactionId,
-      //     type,
-      //     //! ACOMODAR ESTO !!!
-      //     couponId: 0,
-      //     discount: 0,
-      //     deliveryDate: '',
-      //     description: '',
-      //   });
-
-      //! ELIMINO LOS ITEMS CUANDO TERMINA ESTE FLUJO
-      //   await this.prisma.checkout_items.deleteMany({
-      //     where: { userId: data.customer.identification },
-      //   });
     } catch (e) {
       throw new HttpException(e.message, HttpStatus.INTERNAL_SERVER_ERROR);
     }
@@ -127,14 +84,6 @@ export class MobbexService {
         await this.productsService.findCheckoutProducts(items, priceList);
       const total = this.calculateTotal(mobbexItems, discount);
 
-      //! DEPRECADO ?
-      //? Guardo los items temporalmente en la tabla checkout_items para recuperarlos con el webhook
-      //   items.map(async (i) => {
-      //     await this.prisma.checkout_items.create({
-      //       data: { productId: i.productId, productQty: i.qty, userId: userId },
-      //     });
-      //   });
-
       //? Creo una orden temporal para confirmar una vez hecho el pago. (en checkoutWebhook)
       const orderId = await this.usersService.createOrder({
         items,
@@ -148,8 +97,6 @@ export class MobbexService {
       });
 
       console.log('ORDERID + ', orderId);
-
-      //! VER COMO RECUPERAR ESTA ORDEN `TEMPORAL` CREADA
 
       return {
         webhook: 'https://rfddevelopment.tech/mobbex/webhook',
